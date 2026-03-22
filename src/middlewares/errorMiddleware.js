83b5556cc5b86043531a8b4484a53a669b1d2019
gpc186 <gpc186@gmail.com>
2026-03-22 13:38:01 -0300

    Adicionando tratamento de erros com middleware e um constructor novo do Error() para suportar status code

---
+++ src/middlewares/errorMiddleware.js
@@ -0,0 +1,29 @@
+const AppError = require("../utils/AppError");
+/**
+ * 
+ * @param {Error} err 
+ * @param {req} req 
+ * @param {res} res 
+ * @param {next} next 
+ * @returns {Error} Erro com o status e mensagem setada pelo `AppError()`
+ * @example
+ * router.post('/', errorMiddleware, leituraController.store);
+ */
+function errorMiddleware(err, req, res, next) {
+    if(err instanceof AppError){
+        return res.status(err.statusCode).json({ mensagem: err.message})
+    };
+
+    if(err.code === "P2025"){
+        return res.status(404).json({mensagem: "Registro não encontrado!"});
+    };
+
+    if(err.code === "P2002"){
+        return res.status(409).json({mensagem: "registro Já existente!"});
+    };
+
+    console.error(err);
+    return res.status(500).json({mensagem: "Erro interno do servidor!"});
+};
+
+module.exports = errorMiddleware;
