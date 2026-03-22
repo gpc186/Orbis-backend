115b1317a5740516e1b5b2210ec512383af2add6
gpc186 <gpc186@gmail.com>
2026-03-22 13:08:22 -0300

    Criando model para refeshtoken e adicionando comentários para melhor visualização do código

---
+++ src/models/refreshTokenModel.js
@@ -0,0 +1,37 @@
+const prisma = require('../prisma/prisma')
+
+class RefreshTokenModel {
+    /**
+     * Cria um novo refreshToken no banco de dados, ele pega o id do usuário, token novo com informação
+     * @param {number} usuarioId 
+     * @param {string} token 
+     * @param {string} expiresAt
+     * @returns {Promise<Object>} usuarioId, token, expiresAt}
+     * @example
+     * const tokenNovo = await RefreshTokenModel.create({usuarioId, token, expiresAt});
+    */
+    static async create({ usuarioId, token, expiresAt}){
+        return await prisma.refreshToken.create({ data: {usuarioId, token, expiresAt}});
+    };
+    /**
+     * Acha o refreshToken pelo próprio refreshToken
+     * @param {string} token 
+     * @returns {Promise<Object>} usuarioId, token, expiresAt
+     * @example
+     * const usuarioPeloToken = await RefreshTokenModel.findByToken(token);
+     */
+    static async findByToken(token){
+        return await prisma.refreshToken.findUnique({ where: {token}});
+    };
+    /**
+     * Pega o refreshToken e deleta ele do banco de dados
+     * @param {string} token 
+     * @example
+     * await RefreshTokenModel.delete(token);
+     */
+    static async delete(token){
+        return await prisma.refreshToken.delete({ where: {token} })
+    };
+};
+
+module.exports = RefreshTokenModel;
--- src/models/usuárioModel.js
+++ src/models/usuárioModel.js
@@ -1,18 +1,48 @@
 const prisma = require("../prisma/prisma");
 
 class UsuarioModel {
+    /**
+     * Uma função que pega os parametros necessários para criar uma conta e salva no banco de dados
+     * @typedef {Object} Usuario
+     * @param {string} nome
+     * @param {string} email
+     * @param {string} senha
+     * @param {string} role
+     * @returns {Promise<Usuario|null>} Todos os dados anteriores, menos a senha
+     * @example
+     * const usuarioNovo = await UsuarioModel.create({nome, email, senha, role});
+     */
     static async create({ nome, email, senha, role }) {
         return await prisma.usuario.create({ data: { nome, email, senha, role }, omit: { senha: true } });
     };
-
+    /**
+     * Pega os dados de um usuário pelo e-mail, normalmente usado no login
+     * @param {string} email 
+     * @returns {Promise<Usuario|null>} Todos os dados do usuário
+     * @example
+     * const usuario = await UsuarioModel.findByEmail(email)
+     */
     static async findByEmail(email) {
         return await prisma.usuario.findUnique({ where: { email } });
     };
-
+    /**
+     * Pega os dados de usuário pelo id dele, normalmente usado em requisições de api
+     * @param {number} id 
+     * @returns {Promise<Usuario|null>} Todos os dados do usuário, menos a senha
+     * @example
+     * const usuario = await UsuarioModel.findById(id);
+     */
     static async findById(id) {
         return await prisma.usuario.findUnique({ where: { id }, omit: { senha: true } })
     };
-
+    /**
+     * Pega todos os usuários que estão ativos com uma função de paginação, tendo o `skip`e `take` para fazer esta função
+     * @param {number} skip 
+     * @param {number} take 
+     * @returns {Promise<Usuario|null>} Todos os usuários e seus dados, apenas removendo a senha
+     * @example
+     * const todosUsuarios = await UsuarioModel.findAll(skip, take);
+     */
     static async findAll(skip, take) {
         return await prisma.usuario.findMany({
             where: { ativo: true },
@@ -22,17 +52,38 @@ class UsuarioModel {
             omit: { senha: true }
         });
     };
-
+    /**
+     * Atualiza campos requisitados pelo service no banco de dados
+     * @param {number} id
+     * @throws {Error}
+     * @returns {Promise<Usuario|null>} Todos os dados do usuário, menos a senha
+     * @example
+     * const dadosNovos = await UsuarioModel.update({id, dados});
+     */
     static async update({ id, dados }) {
         return await prisma.usuario.update({ where: { id }, data: dados, omit: { senha: true } });
     };
-
-    static async delete({ id }) {
+    /**
+     * Deleta um usuario pelo seu ID
+     * @param {number} id
+     * @example
+     * await UsuarioModel.delete(id)
+     */
+    static async delete(id) {
         return await prisma.usuario.delete({ where: { id } });
     };
-
+    /**
+     * Pega o numero de Admins, isso é pela regra de negócio para sempre ter pelo menos `um admin ativo sempre`
+     * @returns {number} O numero de admins
+     * @example
+     * const adminCount = await UsuarioModel.countAdmins();
+     * 
+     * if(adminCount <= 1){
+     *  throw new AppError("Não é possivel deletar o ultimo admin", 409)
+     * }
+     */
     static async countAdmins() {
-        return await prisma.usuario.count({where: { role: "ADMIN", ativo: true }, omit: {senha: true}});
+        return await prisma.usuario.count({ where: { role: "ADMIN", ativo: true }, omit: { senha: true } });
     };
 };
 
