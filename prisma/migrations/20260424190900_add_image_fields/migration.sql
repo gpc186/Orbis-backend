fe27d3417ea1ea64da25b126544f44fc2d6d2fb2
gpc186 <gpc186@gmail.com>
2026-04-24 16:10:03 -0300

    feat: add image fields to Usuario and Maquina models, update schema and migrations

---
+++ prisma/migrations/20260424190900_add_image_fields/migration.sql
@@ -0,0 +1,7 @@
+-- AlterTable
+ALTER TABLE "Maquina" ADD COLUMN     "caminhoImagem" TEXT,
+ADD COLUMN     "imagem" TEXT;
+
+-- AlterTable
+ALTER TABLE "Usuario" ADD COLUMN     "caminhoFoto" TEXT,
+ADD COLUMN     "fotoPerfil" TEXT;
