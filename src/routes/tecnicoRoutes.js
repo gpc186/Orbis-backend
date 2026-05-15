d803c8794a2fa049540c292045ed92c0efe151e3
gpc186 <gpc186@gmail.com>
2026-05-15 15:31:20 -0300

    Deixando tecnicos ver tecnicos

--- src/routes/tecnicoRoutes.js
+++ src/routes/tecnicoRoutes.js
@@ -1,11 +1,10 @@
 const express = require('express');
 const authMiddleware = require('../middlewares/authMiddleware');
-const roleMiddleware = require('../middlewares/roleMiddleware');
 const TecnicoController = require('../controllers/tecnicoController');
 const router = express.Router();
 
-router.get('/', authMiddleware, roleMiddleware("ADMIN"), TecnicoController.list);
-router.get('/:id', authMiddleware, roleMiddleware("ADMIN"), TecnicoController.findById);
-router.get('/:id/alertas', authMiddleware, roleMiddleware("ADMIN"), TecnicoController.findAlertasByTecnico);
+router.get('/', authMiddleware, TecnicoController.list);
+router.get('/:id', authMiddleware, TecnicoController.findById);
+router.get('/:id/alertas', authMiddleware, TecnicoController.findAlertasByTecnico);
 
 module.exports = router;
